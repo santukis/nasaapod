8f318086aa2779db024eb089d88450f4fc58cff1
David <santamadavid@gmail.com>
2020-04-15 17:09:54 +0200

    Removed unnecessary classes and interfaces

--- app/src/main/java/com/frikiplanet/nasaapod/core/domain/EitherCallback.java
+++
@@ -1,6 +0,0 @@
-package com.frikiplanet.nasaapod.core.domain;
-
-public interface EitherCallback<Error, Success> {
-
-    void execute(Error error, Success success);
-}
--- app/src/main/java/com/frikiplanet/nasaapod/core/domain/LegacyCallback.java
+++
@@ -1,8 +0,0 @@
-package com.frikiplanet.nasaapod.core.domain;
-
-public interface LegacyCallback<Item> {
-
-    void onSuccess(Item item);
-
-    void onError(String error);
-}
